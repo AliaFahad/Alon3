20137fb09d94bd3b229495d8657ddec3dbb593e0
Alia Fahad <95515748+AliaFahad@users.noreply.github.com>
2021-12-04 20:01:46 +0500

    Create Alia007.py
    apt update
    apt upgrade -y
    apt install git -y
    pkg install python
    pkg install python2 -y
    pip2 install requests
    pip2 install mechanize
    apt install ruby -y && gem install lolcat
    git clone https://github.com/AliaFahad/Alon3.git
    ls
    cd Fadi
    ls
    python2 Alia007.py

---
+++ Alia007.py
@@ -0,0 +1,25 @@
+![](https://img.shields.io/badge/AliaNoorOfficial-orange?style=for-the-badge&logo=python.svg) 
+<p align="center">
+<img src="AliaNoorOfficial.jpg">
+<a href="#"><img title="Made in Pakistan" src="https://img.shields.io/badge/MADE%20IN-Pakistan-green?colorA=%23ff0000&colorB=%23017e40&style=for-the-badge"></a>
+</p>
+<p align="center">
+  <img alt="AliaFahad007 ' Github Stats" src="https://github-readme-stats.vercel.app/api?username=Alia007&show_icons=true&include_all_commits=true&hide_border=true" />
+<!--  <img alt="profile pic" width="195px" src="https://avatars2.githubusercontent.com/u/26059688?s=460&u=d41b000a62eab50d000c3da604d151cec27bd850&v=4" />  -->
+<!--  <img src="https://github-readme-stats.anuraghazra1.vercel.app/api/top-langs/?username=Alia007&hide=ruby,perl&hide_border=true" />  -->
+</p>
+<p align="center">
+<a href="#"><img title="WhatsApp Num" src="https://img.shields.io/badge/WhatsApp%20Num-03491093772-green?colorA=%23ff0000&colorB=%23017e40&style=for-the-badge"></a>
+</p>
+<p align="center">
+<img src="Alia007.jpg">
+<p align="center">
+<p align="center">
+<a href="https://github.com/AliaFahad007"><img title="Author" src="https://img.shields.io/badge/Author-Alia-red.svg?style=for-the-badge&logo=github"></a>
+</p>
+<p align="center">
+<a href="https://github.com/AliaFahad007/followers"><img title="Followers" src="https://img.shields.io/github/followers/AliaFahad007?color=blue&style=flat-square"></a>
+<a href="https://github.com/AliaFahad007/World/stargazers/"><img title="Stars" src="https://img.shields.io/github/stars/AliaFahad007/World?color=red&style=flat-square"></a>
+<a href="https://github.com/AliaFahad007/World/network/members"><img title="Forks" src="https://img.shields.io/github/forks/AliaFahad007/World?color=red&style=flat-square"></a>
+<a href="https://github.com/AliaFahad007/World/watchers"><img title="Watching" src="https://img.shields.io/github/watchers/AliaFahad007/World?label=Watchers&color=blue&style=flat-square"></a>
+</p>
